ec7ef08dc34c75402212235d3c1abc8ea44dd840
Kevin Groß <kg@naymspace.de>
2016-04-06 09:41:41 +0200

    Finish static pages prototype

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | SampleApp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | SampleApp"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | SampleApp"
   end
 
 end
