ae1f12df1f307ecf5e7fc4f96c06b06091b69755
Kevin Groß <kg@naymspace.de>
2016-04-06 10:45:49 +0200

    Edit Home Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   def setup
-  	@base_title = " | SampleApp"
+  	@base_title = "SampleApp"
   end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home#{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help#{@base_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About#{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact#{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
