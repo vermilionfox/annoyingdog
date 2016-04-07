64715bb6ebd4f21df6577ac106b03b0dbdb99136
Kevin Groß <kg@naymspace.de>
2016-04-07 16:58:39 +0200

    Refactor users_helper and add users_signup tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,10 +11,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation:  "dog" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
-    get signup_path
+    get signup_path    
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: {  name: "Barkman",
                                             email: "bark@man.com",
@@ -22,5 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation:  "bababark" }
     end
     assert_template 'users/show'
+    assert_select 'div.alert-success'
+    assert flash[:success]
   end
 end
