e1d45d2dd6417ee747839761aad8523c683227ed
Kevin Groß <kg@naymspace.de>
2016-04-08 17:09:29 +0200

    Add pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "'s Dog",
+             email: "example@annoyingdog.org",
+             password:              "barkbark",
+             password_confirmation: "barkbark")
+
+30.times do |n|
+  name  = Faker::Name.name
+  email = "dog-#{n+1}@annoyingdog.org"
+  password = "barkbark"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
